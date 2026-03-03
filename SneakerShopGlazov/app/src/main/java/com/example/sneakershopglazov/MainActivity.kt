1ec1b4e7066dcd4653bca4bb9b1fbc86bf45f88c
Glazov <glazov748@mail.ru>
2026-03-03 11:36:33 +0300

    Change ForgotPassword

--- SneakerShopGlazov/app/src/main/java/com/example/sneakershopglazov/MainActivity.kt
+++ SneakerShopGlazov/app/src/main/java/com/example/sneakershopglazov/MainActivity.kt
@@ -11,13 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.sneakershopglazov.ui.theme.UpGlazovTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-        }
+        setContent {}
     }
 }
 
--- SneakerShopGlazov/app/src/main/java/com/example/sneakershopglazov/ui/view/ForgotPassword.kt
+++ SneakerShopGlazov/app/src/main/java/com/example/sneakershopglazov/ui/view/ForgotPassword.kt
@@ -1,63 +1,137 @@
 package com.example.sneakershopglazov.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.sneakershopglazov.R
+import com.example.sneakershopglazov.ui.theme.UpGlazovTheme
+import com.example.sneakershopglazov.ui.viewModel.ForgotPasswordViewModel
 
 @Composable
-fun ForgotPassword(modifier: Modifier = Modifier) {
+fun ForgotPassword(
+    navController: NavController,
+    viewModel: ForgotPasswordViewModel = viewModel()
+) {
+    var email by remember { mutableStateOf("") }
+    val showDialog = viewModel.showDialog.value // Диалог "Проверьте ваш Email"
 
-    var email by remember { mutableStateOf(value = "") }
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { viewModel.showDialog.value = false },
+            containerColor = Color.White,
+            icon = {
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(RoundedCornerShape(24.dp))
+                        .background(Color(0xFF48B2E7)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(painter = painterResource(id = R.drawable.email_icon), contentDescription = null, tint = Color.White) // Нужна иконка email
+                }
+            },
+            title = {
+                Text("Проверьте Ваш Email", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            },
+            text = {
+                Text(
+                    "Мы Отправили Код Восстановления Пароля На Вашу Электронную Почту.",
+                    textAlign = TextAlign.Center,
+                    color = Color.Gray
+                )
+            },
+            confirmButton = {
+                // При нажатии переходим на OTP экран
+                // Передаем тип "recovery", чтобы OTP экран знал, что мы восстанавливаем пароль
+                Button(
+                    onClick = {
+                        viewModel.showDialog.value = false
+                        navController.navigate("verifyOTP/$email/recovery")
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF48B2E7))
+                ) {
+                    Text("ОК")
+                }
+            }
+        )
+    }
 
-    val context = LocalContext.current
-    Column(modifier = Modifier.fillMaxSize().padding(20.dp), verticalArrangement = Arrangement.Center) {
-        Spacer(modifier = Modifier.weight(1f))
-        Row(modifier = Modifier.fillMaxWidth()){
-            Spacer(modifier = Modifier.width(65.dp))
-            Text(modifier = Modifier.fillMaxWidth(), text = "Забыл Пароль", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+    Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+        ) {
+            Spacer(modifier = Modifier.height(50.dp))
+            // Кнопка Назад
+            Box(
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color(0xFFF7F7F7))
+                    .clickable { navController.popBackStack() },
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(painter = painterResource(id = R.drawable.arrow), contentDescription = null, tint = Color.Black)
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+            Text("Забыл Пароль", fontSize = 24.sp, fontWeight = FontWeight.Bold, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("Введите Свою Учетную Запись\nДля Сброса", fontSize = 14.sp, color = Color.Gray, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+
+            Spacer(modifier = Modifier.height(40.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                placeholder = { Text("xyz@gmail.com") },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    unfocusedContainerColor = Color(0xFFF7F7F7),
+                    focusedContainerColor = Color(0xFFF7F7F7),
+                    unfocusedBorderColor = Color.Transparent,
+                    focusedBorderColor = Color.Transparent
+                )
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+            Button(
+                onClick = {
+                    // Вызываем ViewModel для отправки письма
+                    viewModel.sendRecoveryEmail(email)
+                },
+                modifier = Modifier.fillMaxWidth().height(50.dp),
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF48B2E7))
+            ) {
+                Text("Отправить")
+            }
         }
-        Spacer(modifier = Modifier.height(10.dp))
-        Row(modifier = Modifier.fillMaxWidth()){
-            Spacer(modifier = Modifier.width(20.dp))
-            Text(modifier = Modifier.fillMaxWidth(), text = "Введите свою учетную запись для сброса", color = Color.Gray, fontSize = 16.sp)
-        }
-        Spacer(modifier = Modifier.height(60.dp))
-        Text(text = "Email", color = Color.Gray, fontSize = 16.sp)
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = email, onValueChange = {email = it})
-        Spacer(modifier.height(20.dp))
-        Button(onClick = {
-        }, shape = RoundedCornerShape(10.dp),
-            modifier = Modifier.padding(4.dp, 0.dp).fillMaxWidth()) {
-            Text("Отправить", fontSize = 16.sp)
-        }
-        Spacer(modifier.height(400.dp))
     }
 }
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-private fun ForgotPasswordPreview() {
-    ForgotPassword()
+fun ForgotPasswordPreview() {
+    UpGlazovTheme {
+        val navController = rememberNavController()
+        ForgotPassword(navController = navController)
+    }
 }
