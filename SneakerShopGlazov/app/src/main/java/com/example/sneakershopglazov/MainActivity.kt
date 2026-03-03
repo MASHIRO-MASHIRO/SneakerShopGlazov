ebc14150266ec7d69a9f589410a95231d3276b7a
Glazov <glazov748@mail.ru>
2026-03-03 11:48:16 +0300

    Added RegisterAccountButton

--- SneakerShopGlazov/app/src/main/java/com/example/sneakershopglazov/MainActivity.kt
+++ SneakerShopGlazov/app/src/main/java/com/example/sneakershopglazov/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,8 +22,11 @@ import com.example.sneakershopglazov.ui.view.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             UpGlazovTheme {
                 val navController = rememberNavController()
